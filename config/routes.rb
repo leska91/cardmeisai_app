b9b77db255d8e1ec75a88d4a9c0392d991979a68
leska91 <mayu1101u@gmail.com>
2021-02-19 18:13:56 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'orders#index'
-  resources :orders, only: [:index, :new, :create, :destroy, :edit, :update]
+  resources :orders, only: [:index, :new, :create, :destroy, :edit, :update] do
+    member do
+      get 'search'
+    end
+  end
 end
