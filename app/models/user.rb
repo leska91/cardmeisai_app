3dc8fa5a96cb23a4fd508fbd5663264049f6c2ed
leska91 <mayu1101u@gmail.com>
2021-02-21 20:15:21 +0900

    バリデーションとテストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :orders
+  validates :nickname, presence: true, length: { maximum: 10 }
+
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'は英数字混合にしてください'
+
 end
