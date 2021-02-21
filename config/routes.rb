d6f0079a25a532eec3dea396258ed026dcc3ae84
leska91 <mayu1101u@gmail.com>
2021-02-21 15:42:16 +0900

    amountTotal_view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'orders#index'
-  resources :orders, only: [:index, :new, :create, :destroy, :edit, :update] do
-    member do
-      get 'search'
-    end
-  end
+  resources :orders, only: [:index, :new, :create, :destroy, :edit, :update]
 end
