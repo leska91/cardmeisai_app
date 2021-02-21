b63c586a42a6bb0618bb354391aa727d4d6aa917
leska91 <mayu1101u@gmail.com>
2021-02-22 00:39:51 +0900

    バリデーション記述

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :order do
-    
+    date         { 2021-02-04 }
+    amount       { 1980 }
+    category_id  { '2' }
+    memo         { 'memo' }
+    association :user
   end
 end
